257ec23ba2793aae959dd2936c7300baa5ea20e4
dmart77 <dvrazuolas@gmail.com>
2016-04-25 02:11:40 +0300

    Knockout frontend

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,165 +1,84 @@
-// Userlist data array for filling in info box
-var userListData = [];
-
-// DOM Ready =============================================================
-$(document).ready(function() {
-
-    // Populate the user table on initial page load
-    populateTable();
-
-    // Username link click
-    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
-
-    // Add User button click
-    $('#btnAddUser').on('click', addUser);
-
-    // Delete User link click
-    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
-
-});
-
-// Functions =============================================================
-
-// Fill table with data
-function populateTable() {
-
-    // Empty content string
-    var tableContent = '';
-
-    // jQuery AJAX call for JSON
-    $.getJSON( '/users/userlist', function( data ) {
-
-        // Stick our user data array into a userlist variable in the global object
-        userListData = data;
-
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-            tableContent += '</tr>';
+var UserListModel = function() {
+    
+    var self = this;
+    self.users = ko.observableArray([]);
+    self.current = ko.observable();
+    self.newUser = ko.mapping.fromJS({username:null, email:null, fullname:null, age:null, location:null, gender:null});
+    
+    self.isNewUserValid = function() {
+        return (self.newUser.username() &&
+                self.newUser.fullname());
+    };
+    
+    self.showUserInfo = function(user) {
+        self.current(user);
+    };    
+    
+    self.loadUsers = function() {
+        
+        $.getJSON('/users/userlist', function( data ) {
+            self.users(data);
+            
+            if (self.users().length > 0)
+                self.current(self.users()[0]);            
         });
+        
+    };
+    
+    self.deleteUser = function(user) {
+        
+        var confirmation = confirm('Are you sure you want to delete this user?');
 
-        // Inject the whole content string into our existing HTML table
-        $('#userList table tbody').html(tableContent);
-    });
-};
+        if (confirmation === true) {
 
-// Show User Info
-function showUserInfo(event) {
+            $.ajax({
+                type: 'DELETE',
+                url: '/users/deleteuser/' + user._id
+            }).done(function(response) {
 
-    // Prevent Link from Firing
-    event.preventDefault();
+                // Check for a successful (blank) response
+                if (response.msg === '') {
+                    
+                    if (user === self.current())
+                        self.current(undefined);
+                        
+                    self.users.remove(user);                    
+                }
+                else {
+                    alert('Error: ' + response.msg);
+                }
 
-    // Retrieve username from link rel attribute
-    var thisUserName = $(this).attr('rel');
-
-    // Get Index of object based on id value
-    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
-
-    // Get our User Object
-    var thisUserObject = userListData[arrayPosition];
-
-    //Populate Info Box
-    $('#userInfoName').text(thisUserObject.fullname);
-    $('#userInfoAge').text(thisUserObject.age);
-    $('#userInfoGender').text(thisUserObject.gender);
-    $('#userInfoLocation').text(thisUserObject.location);
-
-};
-
-// Add User
-function addUser(event) {
-    event.preventDefault();
-
-    // Super basic validation - increase errorCount variable if any fields are blank
-    var errorCount = 0;
-    $('#addUser input').each(function(index, val) {
-        if($(this).val() === '') { errorCount++; }
-    });
-
-    // Check and make sure errorCount's still at zero
-    if(errorCount === 0) {
-
-        // If it is, compile all user info into one object
-        var newUser = {
-            'username': $('#addUser fieldset input#inputUserName').val(),
-            'email': $('#addUser fieldset input#inputUserEmail').val(),
-            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
-            'age': $('#addUser fieldset input#inputUserAge').val(),
-            'location': $('#addUser fieldset input#inputUserLocation').val(),
-            'gender': $('#addUser fieldset input#inputUserGender').val()
+            });
         }
+    };
+    
+    self.addUser = function() {
 
-        // Use AJAX to post the object to our adduser service
         $.ajax({
             type: 'POST',
-            data: newUser,
+            data: ko.mapping.toJS(self.newUser),
             url: '/users/adduser',
             dataType: 'JSON'
-        }).done(function( response ) {
+        }).done(function(response) {
 
             // Check for successful (blank) response
             if (response.msg === '') {
-
-                // Clear the form inputs
-                $('#addUser fieldset input').val('');
-
-                // Update the table
-                populateTable();
-
-            }
-            else {
-
-                // If something goes wrong, alert the error message that our service returned
-                alert('Error: ' + response.msg);
-
-            }
-        });
-    }
-    else {
-        // If errorCount is more than 0, error out
-        alert('Please fill in all fields');
-        return false;
-    }
-};
-
-// Delete User
-function deleteUser(event) {
-
-    event.preventDefault();
-
-    // Pop up a confirmation dialog
-    var confirmation = confirm('Are you sure you want to delete this user?');
-
-    // Check and make sure the user confirmed
-    if (confirmation === true) {
-
-        // If they did, do our delete
-        $.ajax({
-            type: 'DELETE',
-            url: '/users/deleteuser/' + $(this).attr('rel')
-        }).done(function( response ) {
-
-            // Check for a successful (blank) response
-            if (response.msg === '') {
+                self.loadUsers();
             }
             else {
                 alert('Error: ' + response.msg);
             }
-
-            // Update the table
-            populateTable();
-
-        });
-
-    }
-    else {
-
-        // If they said no to the confirm, do nothing
-        return false;
-
-    }
-
+        });        
+    };    
+    
 };
+
+// DOM Ready =============================================================
+
+$(document).ready(function() {
+    
+    var userListModel = new UserListModel();
+    userListModel.loadUsers();    
+    ko.applyBindings(userListModel);
+    
+});
